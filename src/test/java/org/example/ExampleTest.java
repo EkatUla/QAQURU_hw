8a10ec21d52d09cc7a1814a52182cae96f99c9f0
EkatUla2022 <reikon@list.ru>
2022-09-22 01:48:52 +0300

    one test

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class ExampleTest {
+    @Test
+    void ExampleTest(){
+        Assertions.assertTrue(2>1);
+
+    }
+}
