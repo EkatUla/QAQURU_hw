a7f2f0cc4311bd107b3fa6a23c7c569b0e5b84c6
EkatUla2022 <reikon@list.ru>
2022-09-22 02:24:50 +0300

    first try

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -2,16 +2,18 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Random;
+
 
 public class ExampleTest {
     @Test
     void ExampleTest(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
 
     }
     @Test
     void ExampleTest1(){
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(new Random().nextInt() > 2);
 
     }
 }
